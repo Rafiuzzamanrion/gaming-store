576e548e959825d4f719ef2be1f80b90862df1de
Rafiuzzamanrion <rafiujjaman5683@gmail.com>
2025-11-02 23:53:33 +0600

    added the rajdhani font

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,22 +1,31 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import type {Metadata} from 'next';
+import {Rajdhani} from 'next/font/google';
 import '@/styles/globals.css';
-
-const inter = Inter({ subsets: ['latin'] });
+import clsx from "clsx";
 
 export const metadata: Metadata = {
   title: 'Gaming Store',
   description: 'Your ultimate gaming destination',
 };
-
+const font = Rajdhani({
+  subsets: ['latin'],
+  variable: '--font-rajdhani',
+  weight: ['400', '500', '600', '700'],
+});
 export default function RootLayout({
-  children,
-}: {
+                                     children,
+                                   }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning={true}>
+    <body
+      suppressHydrationWarning={true}
+      className={clsx("min-h-screen text-foreground bg-background font-sans antialiased", font.variable,)}>
+   <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
+     {children}
+   </main>
+    </body>
     </html>
   );
 }
