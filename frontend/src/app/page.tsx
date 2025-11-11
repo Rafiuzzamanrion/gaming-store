2bff726766f6204be72d2acb71bc4aa6959928c0
Rafiuzzamanrion <rafiujjaman5683@gmail.com>
2025-11-11 18:50:39 +0600

    added some image and added custom class

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <p className="mt-4 text-xl">Welcome to your gaming destination!</p>
+      <p className="mt-4 text-xl text-primary">Welcome to your gaming destination!</p>
     </main>
   );
 }
