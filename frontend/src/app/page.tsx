9593d0c27af4a1b06702e54bcace29f02d5adc25
Rafiuzzamanrion <rafiujjaman5683@gmail.com>
2025-11-12 22:52:16 +0600

    added all logo image

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,7 +2,7 @@
 import {Button} from "@heroui/react"
 export default function Home() {
   return (
-    <main className="flex h-[300px] w-32 flex-col items-center justify-center p-24 bg-card rounded-lg shadow-lg">
+    <main className="flex h-[300px] w-32 flex-col text- items-center justify-center p-24 bg-card rounded-lg shadow-lg">
       <h1 className="text-4xl font-bold text-primary">Gaming Store</h1>
       <p className="mt-4 text-xl text-secondary">Welcome to your gaming destination!</p>
       <Button color={'success'} className="mt-6">
