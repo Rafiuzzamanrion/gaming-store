93f341a38b556489ce8ad4749041cff041536965
Rafiuzzamanrion <rafiujjaman5683@gmail.com>
2025-11-13 00:42:44 +0600

    created site config

---
+++ frontend/src/config/siteConfig.ts
@@ -0,0 +1,56 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Nexus Gaming",
+  description: "Your ultimate gaming destination.",
+  navItems: [
+    {
+      label: "HOME",
+      href: "/",
+    },
+    {
+      label: "SHOP",
+      href: "/shop",
+    },
+    {
+      label: "ABOUT US",
+      href: "/about",
+    },
+    {
+      label: "CONTACT US",
+      href: "/contact",
+    },
+  ],
+  navMenuItems: [
+    {
+      label: "HOME",
+      href: "/",
+    },
+    {
+      label: "SHOP",
+      href: "/shop",
+    },
+    {
+      label: "ABOUT US",
+      href: "/about",
+    },
+    {
+      label: "CONTACT US",
+      href: "/contact",
+    },
+
+  ],
+  links: {
+    github: "https://github.com/heroui-inc/heroui",
+    twitter: "https://twitter.com/hero_ui",
+    docs: "https://heroui.com",
+    discord: "https://discord.gg/9b6yyZKmH4",
+    sponsor: "https://patreon.com/jrgarciadev",
+    instagram: "https://instagram.com/hero_ui",
+    linkedin: "https://linkedin.com/company/heroui",
+    telegram: "https://t.me/VerifiedBmService",
+    facebook: "https://www.facebook.com/profile.php?id=100077270117771",
+    whatsapp:"https://wa.me/+8801820861283"
+
+  },
+};
