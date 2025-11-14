fd353d1c84ee875d622992b89368f13fad4dcdd1
Rafiuzzamanrion <rafiujjaman5683@gmail.com>
2025-11-14 22:17:12 +0600

    updated the custom card color

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex h-[300px] w-32 flex-col text- items-center justify-center p-24 bg-card rounded-lg shadow-lg">
       <h1 className="text-4xl font-bold text-primary">Gaming Store</h1>
-      <p className="mt-4 text-xl text-secondary">Welcome to your gaming destination!</p>
+      <p className="mt-4 text-xl flex">Welcome to your gaming destination!</p>
       <Button color={'success'} className="mt-6">
         Get Started
       </Button>
